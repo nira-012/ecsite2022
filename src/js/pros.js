37df687ee31e3004f7758ba69b2f5629b02facdf
nira-012 <horii.nira12@gmail.com>
2022-08-02 09:32:41 +0900

    sucsess
    sucsess

--- src/js/pros.js
+++ src/js/pros.js
@@ -18,3 +18,13 @@ if (out.id == "FALSE") {
 else {
     out.innerText = "";
 }
+
+
+
+let soldOut = document.querySelectorAll(".items__out");
+console.log(soldOut[0].alt)
+for (let i = 0; i < soldOut.length; i++) {
+    if (soldOut[i].alt == "TRUE") {
+        soldOut[i].setAttribute("style", "opacity:0");
+    }
+}
